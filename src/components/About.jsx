cf54448af678f2e02f0a4402d1d0f85c30f3e9e8
Archit <archit499kul@gmail.com>
2023-10-03 12:05:53 +0530

    tech section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,10 +11,10 @@ const ServiceCard = ({ title, icon, index }) => {
   return (
     <Tilt
       className="xs:w-[250px] w-full"
-      tiltMaxAngleX={30}
-      scale={1.1}
-      tiltMaxAngleY={30}
-      transitionSpeed={1500}>
+      tiltMaxAngleX={25}
+      scale={1.05}
+      tiltMaxAngleY={25}
+      transitionSpeed={1800}>
       <motion.div
         variants={fadeIn("right", "spring", index * 0.5, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
@@ -39,16 +39,16 @@ const About = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm a skilled developer with experience in JavaScript and expertise in
+        frameworks like React, Node.js, and Three.js. I'm a quick learner and
+        collaborate closely with clients to create efficient, scalable, and
+        user-friendly solutions that solve real-world problems. Let's work
+        together to bring your ideas to life!
       </motion.p>
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
